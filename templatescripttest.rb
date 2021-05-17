d0f31a89bec01bd02546d89e0e7fb93eb884d660
jacoabreuzup <joao.abreu@zup.com.br>
2021-05-17 11:11:31 -0300

    add formula

---
+++ templatescripttest.rb
@@ -0,0 +1,12 @@
+class Templatescripttest < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/jacoabreuzup/templateScriptTest/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "57f51751ec804082f45fe618242eb75dd3d1a4a7539f5d1a0330f44cbba72f6f"
+  license ""
+
+  def install
+      bin.install "ios-templates"
+  end
+
+end
